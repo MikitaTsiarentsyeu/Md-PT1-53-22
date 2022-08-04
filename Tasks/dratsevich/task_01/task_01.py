1d24ab70691293e4692629ca976e7e29b55d25b8
= <k.dratsevich@gmail.com>
2022-08-04 23:13:31 +0300

    make task_01

---
+++ Tasks/dratsevich/task_01/task_01.py
@@ -0,0 +1,12 @@
+
+amount = float(input('input amount of money \n'))
+time = int(input('input time in years \n'))
+rate_deposit = float(input('input rate deposit \n'))
+capitalisation = input('do you want to enable monthly capitalisation? (yes/no)')
+if capitalisation == 'yes':
+    money = round(amount * pow((1 + rate_deposit/1200),(time * 12)),2)
+    print(f'in the end of deposit you will get back {money}, your profit is {money - amount}')
+elif capitalisation == 'no':
+    money = round(amount + amount * rate_deposit / 100 * time ,2)
+    print(f'in the end of deposit you will get back {money}, your profit is {money - amount}')
+
