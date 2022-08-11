f26c67b7b20f9b33285fcc27c5d24bd65a2f754f
Elizabeth <105500510+ElizabethG13@users.noreply.github.com>
2022-08-11 14:16:51 +0300

    Update Task1.py

--- Tasks/Galievskaya/Task1/Task1.py
+++ Tasks/Galievskaya/Task1/Task1.py
@@ -1,6 +1,8 @@
-P0 = int(input("Input deposit amount: \n"))
-t = float(input("Input term of deposit in years: \n")) 
-r = float(input("Input annual interest rate: \n"))
+from decimal import Decimal 
+
+P0 = Decimal(input("Input deposit amount: \n"))
+t = Decimal(input("Input term of deposit in years: \n")) 
+r = Decimal(input("Input annual interest rate: \n"))
 
 if (P0<=0 or t<=0 or r<=0):
     print("Oops, looks like you entered invalid data, please try again.")
@@ -11,9 +13,9 @@ r0 = r / 100
 
 if (mc.lower() == "n"):
     x = P0 * r0 * t
-    Px = P0 + x
+    Px = round(P0 + x, 2)
 else:
     n = 12
-    Px = round( (P0 * (1 + r0/n)**(n*t)), 2)
+    Px = round(P0 * (1 + r0/n)**(n*t), 2)
 
 print(f"Amount at the end of the period: {Px}")
