979eaf9d8d4b76e931b5aa4d60369e3e4bfd7833
Ilyachernousik <ilyachernousik1129@gmail.com>
2022-08-16 12:36:26 +0300

    Create Task1.py

---
+++ Tasks/Chernousik/Task1/Task1.py
@@ -0,0 +1,14 @@
+a = int(input('Enter amount \n'))
+b = int(input('Enter time period(years) \n'))
+c = float(input('Enter rate \n'))                       
+capitalization = input('Enter y or n \n')
+if capitalization == 'n':
+    years_per_day = b * 365
+    money = (a * c * (years_per_day / 365)) / 100
+    money += a
+    print(f'After {b} years you are get {money} glass bottles')        
+elif capitalization == 'y':
+    years_per_day = b * 365
+    term_per_month = b * 12
+    money = int(a * (1 + (c / 100) / 12) ** term_per_month) 
+    print(f'After {b} years you are get {money} glass bottles')
