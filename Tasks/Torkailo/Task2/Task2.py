038782b2476d46ae3a71086197d27472492e7624
Vera <87011232+VeraTorkailo@users.noreply.github.com>
2022-08-25 13:50:28 +0300

    Update Task2.py
    Used formatted string for print result

--- Tasks/Torkailo/Task2/Task2.py
+++ Tasks/Torkailo/Task2/Task2.py
@@ -20,13 +20,13 @@ d = {9103976271:[("Reina", "Meinhard"), ("Memphis", "Tennessee")],
 6123479367:[("Lorenza", "Takuya"), ("Indianapolis", "Indiana")],
 7548993768:[("Margarete", "Quintin"), ("Raleigh", "North Carolina")]}
 
-ph_num = input('Enter phone number ')
+ph_num = input("Enter phone number ")
 
-if ph_num.isdigit() !=True or len(str(ph_num))!= 10:
-    print('Enter integer number with 10 symbols length')
+if ph_num.isdigit() !=True or len(ph_num)!= 10:
+    print("Enter integer number with 10 symbols length")
 elif d.get(int(ph_num)) == None:
     print("Phone number wasn't found")
 else:
-    print (d.get(int(ph_num))[0][0], d.get(int(ph_num))[0][1], 'from', d.get(int(ph_num))[1][0], d.get(int(ph_num))[1][1] , sep = ' ', end = '. \n' )
+    ph_num = int(ph_num)
+    print (f"{d.get(ph_num)[0][0]} {d.get(ph_num)[0][1]} from {d.get(ph_num)[1][0]}, {d.get(ph_num)[1][1]}.")
 
-# find out how to print the result easily
