ea4c51608f123f4c7f39d8f5c7f2bdc117984b50
Vera <87011232+VeraTorkailo@users.noreply.github.com>
2022-08-25 14:16:55 +0300

    Update Task1.py
    Rewrite using decimal + formatted string

--- Tasks/Torkailo/Task1/Task1.py
+++ Tasks/Torkailo/Task1/Task1.py
@@ -1,21 +1,22 @@
+import decimal
 import math
 
-first_sum=int(input('Enter initial deposit amount (ex. 1000) '))
+first_sum=decimal.Decimal(input('Enter initial deposit amount (ex. 1000) '))
 year=int(input('Enter deposit terms in years (ex. 5) '))
-perc=int(input('Enter annual deposit persentage (ex. 15) '))
+perc=decimal.Decimal(input('Enter annual deposit persentage (ex. 15) '))
 cap=str(input('Press "Y" - to enable monthly capitalization or Press "N" - to disable monthly capitalization '))
 
 if cap.upper() !='Y' and cap.upper() !='N':
     print ('Error: try again - press Y or N')
 else:
-    print ('Initial deposit amount', first_sum, sep = ' ', end = '. \n')
-    print ('Deposit terms in years', year, sep = ' ', end = '. \n')
-    print ('Annual deposit persentage', perc, sep = ' ', end = '. \n')
+    print (f'Initial deposit amount {first_sum} BYN')
+    print (f'Deposit terms in years {year} years.')
+    print (f'Annual deposit persentage {perc}%')
 
     if cap.upper() == 'Y':
-        print ('Monthly capitalization - YES', sep = ' ', end = '. \n')
-        print ('Total income:' , str(math.ceil(first_sum*((1 + perc / 12 / 100)**(year*12)))), sep = ' ', end = '.')
+        print ('Monthly capitalization - YES')
+        print (f'Total income: {round(first_sum*((1 + perc / 12 / 100)**(year*12)),2)} BYN')
     else:
-        print ('Monthly capitalization - NO', sep = ' ', end = '. \n')
-        print ('Total income:' , str(math.ceil(first_sum * (1 + perc / 100*year))), sep = ' ', end = '.')
+        print ('Monthly capitalization - NO')
+        print (f'Total income: {round(first_sum *(1 + perc / 100*year),2)} BYN')
         
