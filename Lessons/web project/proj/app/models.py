cff832f762f3fbe291ea6d7084d52c70c6ce2481
Mikita Tsiarentsyeu <Mikita_Tsiarentsyeu@epam.com>
2022-10-06 19:49:39 +0600

    add lesson 06/10

--- Lessons/web project/proj/app/models.py
+++ Lessons/web project/proj/app/models.py
@@ -1,3 +1,20 @@
+from distutils.command.upload import upload
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+
+    name = models.CharField(max_length=500, blank=False)
+    email = models.EmailField(blank=False, primary_key=True)
+
+class Post(models.Model):
+
+    POST_TYPES = [('c', 'copyright'), ('p', 'public')]
+
+    title = models.CharField(max_length=100, blank=False)
+    subtitle = models.CharField(max_length=200)
+    content = models.TextField(blank=False)
+    image = models.ImageField(upload_to="uplods")
+    issued = models.DateTimeField()
+    post_type = models.CharField(max_length=1, choices=POST_TYPES)
+
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
